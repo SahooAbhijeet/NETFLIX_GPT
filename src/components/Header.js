473de4af20e739aeccaa2121b56bc58d78e7c629
ABHIJEET <abhijeetsahoomsd@gmail.com>
2024-01-20 22:48:46 +0530

    fixed error

--- src/components/Header.js
+++ src/components/Header.js
@@ -61,9 +61,7 @@ const Header = () => {
   const handleChangeLanguage = (e) => {
     dispatch(changeLanguage(e.target.value));
   }
-const handleTheme = () => {
-  setTheme(theme === "dark" ? "light" : "dark")
-}
+
   return (
     <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between '>
         <img 
